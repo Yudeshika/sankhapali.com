54156ab8e0b5d98c2e0c0713b12bc47631bbb422
Yudeshika Sankhapali <yudeshika@gmail.com>
2024-09-29 21:58:42 +0100

    Fix menu gap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <NavLink to="/" className="w-20 h-10 rounded-lg items-center justify-center flex font-bold shadow-md">
             <p className="blue-gradient_text">Sanke</p>
         </NavLink>
-        <nav className="flex text-lg font-medium gap-7 space-x-4">
+        <nav className="flex text-lg font-medium gap-8 space-x-4">
             <NavLink to="/about" className={ ({ isActive }) => isActive ? 'text-blue-500': 'text-black' }>
                 About
             </NavLink>
